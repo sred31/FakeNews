f04366ff692578e5f88644136cc00efa8508aa0f
eric-hein <44380965+eric-hein@users.noreply.github.com>
2019-03-18 19:58:51 -0700

    Added Array of Resulting Titles/Links
    Also now runs on multiple websites in one execution

--- newsscraper.js
+++ newsscraper.js
@@ -1,16 +1,26 @@
 //Dependency: Npm crawler must be installed before this code can run, using 
 //  "npm install crawler" from the terminal
-//Usage: node newsscraper.js <website link>
-
+//Usage: node newsscraper.js
 
 /*
 *   Known Issues: 
-*      Currently only evaluates h1's, other websites will use other header tags
-*      Results need to be stored in arrays/tables rather than just logged to console
+*       Need to remove duplicate results before they're stored by checking for same link
+*       Need to actually store table of results to Firebase
 */
 var Crawler = require('crawler');
 var wordlist = ["WOMAN", "FOOT", "FACEBOOK", "DATA", "COLLEGE"];
+//var wordlist = ["FIRE", "AVALANCHE", "STORM"];
+//FIXME: May want to use "theguardian.com/world/natural-disasters" instead for the guardian
+var sitelist = ["https://abcnews.go.com", "https://apnews.com/apf-topnews",
+     "https://theguardian.com/world"];
 //var website = 'http://abcnews.go.com/';
+
+//Results is an Array of Objects containing Title/Link pairs for all 
+//  articles with keywords in their titles
+var Results = [];
+
+//NOTE: This section will probably be deleted, but its sticking around for now just in case
+/*
 var website = "";
 if (process.argv.length != 3) {
     console.log("WARNING: Please specify a Keyword and Website");
@@ -18,12 +28,6 @@ if (process.argv.length != 3) {
 }
 process.argv.forEach(function(val,index,array) {
     switch(index) {
-        /*
-        case 2:
-            keyword = val;
-            keyword = keyword.toUpperCase();
-            break;
-        */
         case 2:
             website = val;
             website = "https://" + website;
@@ -33,7 +37,7 @@ process.argv.forEach(function(val,index,array) {
             break;
     }
 });
-
+*/
 
 var c = new Crawler({
     // This will be called for each crawled page
@@ -43,11 +47,9 @@ var c = new Crawler({
             console.log(error);
         }
         else{
-            //var keyword = "trump"
-            //keyword = keyword.toUpperCase();
             var $ = res.$;
             var hit_ctr = 0;
-            var link = "";
+            var this_link = "";
             var listlength = wordlist.length;
 
             //Print formatting
@@ -59,7 +61,33 @@ var c = new Crawler({
 
             // $ is Cheerio by default
             //a lean implementation of core jQuery designed specifically for the server       
-            //For loop iterates through each H1 Header
+            //For loop iterates through each anchor tag
+
+
+            //CODE FOR THE GUARDIAN (And all Websites we're using, I guess?)
+            $('body a').each(function(i, elm){
+                //Storing each element's text into a string, and trimming whitespace
+                var str = $(this).text();
+                str = str.trim();
+                for (var i = 0; i < listlength; i++){
+                    //Converting to Upper Case to make strings case-independent
+                    if (str.toUpperCase().includes(wordlist[i])){
+                        hit_ctr += 1;
+                        //console.log("\nTitle: " + str);
+                        //Storing the <a href> component of that into this_link
+                        this_link = $(this).attr("href");
+                        //console.log("Link: " + this_link);
+
+                        //Pushing title and link of hit to Result array
+                        var hit = {title: str, link: this_link}
+                        Results.push(hit)
+                    }
+                }
+            });
+            
+
+            //CODE FOR ABCNEWS... not sure this is necessary?
+            /*
             $('body h1').each(function(i, elm){
                 //Storing each element's text into a string, and trimming whitespace
                 var str = $(this).text();
@@ -75,6 +103,7 @@ var c = new Crawler({
                     }
                 }
             });
+            */
             console.log("===================================");
             console.log("|                                 |");
             console.log("|            Finished             |");
@@ -85,5 +114,18 @@ var c = new Crawler({
         done();
     }
 });
+
+
 // Queue just one URL, with default callback
-c.queue(website);
+for (var i=0; i < sitelist.length; i++){
+    c.queue(sitelist[i]);
+}
+
+
+//'Drain" event makes following Code only execute when all queued websites have been visited
+c.on('drain', function(){
+    for (var i in Results){
+        console.log(Results[i]);
+    }
+});
+//c.queue(website)
